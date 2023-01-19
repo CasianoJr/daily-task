de552dbaa3e617a8ae61225776d3cd009ab48292
Casiano Fernandez <casianojrfernandez@gmail.com>
2023-01-19 22:10:31 +0800

    feat: hardcode dialog

--- src/pages/configureTaskDialog/ConfigureTaskDialog.tsx
+++ src/pages/configureTaskDialog/ConfigureTaskDialog.tsx
@@ -1,7 +1,39 @@
 import React from "react";
+import { Icon } from "@iconify/react";
 
 const ConfigureTaskDialog = () => {
-  return <div>ConfigureTaskDialog</div>;
+  return (
+    <div className="modal-wrapper">
+      <div className="modal-header">Configure Task</div>
+      <div className="modal-content-wrap">
+        <div className="modal-div">
+          <input className="modal-title-input" value={"Design the app"}></input>
+        </div>
+        <div className="modal-div">
+          <div className="modal-div-1">
+            <span>
+              <Icon icon="mdi:checkerboard" />
+            </span>{" "}
+            <span>Length</span>
+          </div>
+          <input className="modal-length-input" value={"120 minutes"}></input>
+        </div>
+        <div className="modal-div">
+          <div className="modal-div-1">
+            <span>
+              <Icon icon="mdi:checkerboard" />
+            </span>{" "}
+            <span>Theme</span>
+          </div>
+          <input className="modal-color-input" type="color"></input>
+        </div>
+      </div>
+      <div className="modal-btn-action">
+        <button className="modal-btn-cancel">Cancel</button>
+        <button className="modal-btn-save">Save</button>
+      </div>
+    </div>
+  );
 };
 
 export default ConfigureTaskDialog;
--- src/pages/taskDetailPage/TaskDetailPage.tsx
+++ src/pages/taskDetailPage/TaskDetailPage.tsx
@@ -34,53 +34,60 @@ export const TaskDetailPage = () => {
   if (!task) return null;
 
   return (
-    <div className="detail-wrapper" style={{ backgroundColor: task.theme }}>
-      <div className="detail-row-1">
-        <div className="detail-elapsed-wrap">
-          <div className="col-1-title"> Minitues Elapsed</div>
-          <div className="col-1-timed-wrap">
-            <span>
-              <Icon icon="material-symbols:hourglass-bottom-rounded" width={40} />
-            </span>
-            <span>{Math.floor(task.originalTime - task.runningTime)}</span>
-          </div>
-        </div>
-        <div className="detail-elapsed-left-wrap">
-          <div className="col-1-title"> Minitues Remaining</div>
-          <div className="col-1-timed-wrap">
-            <span>{Math.floor(task.runningTime)}</span>
-            <span>
-              <Icon icon="material-symbols:hourglass-top-rounded" width={40} />
-            </span>
-          </div>
-        </div>
-        <div className="detail-remaining-wrap"></div>
+    <div>
+      <div className="task-create-wrap-btn">
+        <button>
+         Edit
+        </button>
       </div>
-      <div className="detail-row-2">
-        <div className="detail-main-title">{task.title}</div>
-        <div className="detail-main-time">
-          <div className="detail-time">
-            {Math.floor(task.runningTime)} :{" "}
-            {Math.ceil((task.runningTime * 60) % 60)
-              .toString()
-              .padStart(2, "0")}
+      <div className="detail-wrapper" style={{ backgroundColor: task.theme }}>
+        <div className="detail-row-1">
+          <div className="detail-elapsed-wrap">
+            <div className="col-1-title"> Minitues Elapsed</div>
+            <div className="col-1-timed-wrap">
+              <span>
+                <Icon icon="material-symbols:hourglass-bottom-rounded" width={40} />
+              </span>
+              <span>{Math.floor(task.originalTime - task.runningTime)}</span>
+            </div>
           </div>
-          <div className="detail-time-label">
-            <div>Minutes</div>
-            <div>Seconds</div>
+          <div className="detail-elapsed-left-wrap">
+            <div className="col-1-title"> Minitues Remaining</div>
+            <div className="col-1-timed-wrap">
+              <span>{Math.floor(task.runningTime)}</span>
+              <span>
+                <Icon icon="material-symbols:hourglass-top-rounded" width={40} />
+              </span>
+            </div>
+          </div>
+          <div className="detail-remaining-wrap"></div>
+        </div>
+        <div className="detail-row-2">
+          <div className="detail-main-title">{task.title}</div>
+          <div className="detail-main-time">
+            <div className="detail-time">
+              {Math.floor(task.runningTime)} :{" "}
+              {Math.ceil((task.runningTime * 60) % 60)
+                .toString()
+                .padStart(2, "0")}
+            </div>
+            <div className="detail-time-label">
+              <div>Minutes</div>
+              <div>Seconds</div>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="detail-row-3">
-        <button className="detail-stop-btn btn-div" onClick={handleStop}>
-          <Icon icon="material-symbols:stop-circle-outline" width={60} />
-        </button>
-        <button className="detail-play-btn btn-div" onClick={handlePlay}>
-          <Icon icon="material-symbols:play-circle-outline" width={100} />
-        </button>
-        <button className="detail-pause-btn btn-div" onClick={handlePause}>
-          <Icon icon="ic:baseline-pause-circle-outline" width={60} />
-        </button>
+        <div className="detail-row-3">
+          <button className="detail-stop-btn btn-div" onClick={handleStop}>
+            <Icon icon="material-symbols:stop-circle-outline" width={60} />
+          </button>
+          <button className="detail-play-btn btn-div" onClick={handlePlay}>
+            <Icon icon="material-symbols:play-circle-outline" width={100} />
+          </button>
+          <button className="detail-pause-btn btn-div" onClick={handlePause}>
+            <Icon icon="ic:baseline-pause-circle-outline" width={60} />
+          </button>
+        </div>
       </div>
     </div>
   );
