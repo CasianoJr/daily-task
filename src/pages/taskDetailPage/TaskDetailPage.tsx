152239192d2afd34d3b3a74243e5ff8e7f96db4b
Casiano Fernandez <casianojrfernandez@gmail.com>
2023-01-19 13:26:09 +0800

    feat: hardcode detail view

--- src/pages/taskDetailPage/TaskDetailPage.tsx
+++ src/pages/taskDetailPage/TaskDetailPage.tsx
@@ -1,7 +1,51 @@
-import React from 'react'
+import React from "react";
+import { Icon } from "@iconify/react";
 
 export const TaskDetailPage = () => {
   return (
-    <div>TaskDetailPage</div>
-  )
-}
+    <div className="detail-wrapper" style={{ backgroundColor: "yellow" }}>
+      <div className="detail-row-1">
+        <div className="detail-elapsed-wrap">
+          <div className="col-1-title"> Minitues Elapsed</div>
+          <div className="col-1-timed-wrap">
+            <span>
+              <Icon icon="material-symbols:hourglass-bottom-rounded" width={40} />
+            </span>
+            <span>{19}</span>
+          </div>
+        </div>
+        <div className="detail-elapsed-left-wrap">
+          <div className="col-1-title"> Minitues Remaining</div>
+          <div className="col-1-timed-wrap">
+            <span>{110}</span>
+            <span>
+              <Icon icon="material-symbols:hourglass-top-rounded" width={40} />
+            </span>
+          </div>
+        </div>
+        <div className="detail-remaining-wrap"></div>
+      </div>
+      <div className="detail-row-2">
+        <div className="detail-main-title">"Demo taks"</div>
+        <div className="detail-main-time">
+          <div className="detail-time">00: 00</div>
+          <div className="detail-time-label">
+            <div>Minutes</div>
+            <div>Seconds</div>
+          </div>
+        </div>
+      </div>
+      <div className="detail-row-3">
+        <button className="detail-stop-btn btn-div">
+          <Icon icon="material-symbols:stop-circle-outline" width={60} />
+        </button>
+        <button className="detail-play-btn btn-div">
+          <Icon icon="material-symbols:play-circle-outline" width={100} />
+        </button>
+        <button className="detail-pause-btn btn-div">
+          <Icon icon="ic:baseline-pause-circle-outline" width={60} />
+        </button>
+      </div>
+    </div>
+  );
+};
